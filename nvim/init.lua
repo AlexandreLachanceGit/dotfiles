97cc670c11a5645e2bfdd6d1b6797c617a32103b
Lachance, Alexandre <alexandrelachance@me.com>
2023-01-29 18:38:44 -0500

    Update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,11 +1,1 @@
-vim.opt.termguicolors = true
-vim.opt.clipboard = "unnamedplus"
-vim.opt.mouse = "a"
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.g.gruvbox_contrast_dark = "hard"
-vim.cmd("colorscheme gruvbox")
-
-require("plugins")
-require("keybinds")
-require("neovide")
+require("alex")
--- nvim/lua/neovide.lua
+++
@@ -1,6 +0,0 @@
-local vim = vim
-
-if vim.g.neovide then
-    vim.g.neovide_transparency = 0.98
-    vim.opt.guifont = {"Fira Code Retina", ":h11"}
-end
